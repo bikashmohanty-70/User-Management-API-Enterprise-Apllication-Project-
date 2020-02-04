002cf243564ed1e5659ecdf2672d5b37c644bd83
bikashmohanty-70 <bikashmohanty70@gmail.com>
2020-02-04 19:40:25 +0530

    [Bikash Mohanty]add. getUserByMail() used woth POST request

--- loginregistration/src/main/java/com/bridgelabz/userapis/loginregistration/services/UsersRegistration.java
+++ loginregistration/src/main/java/com/bridgelabz/userapis/loginregistration/services/UsersRegistration.java
@@ -1,30 +1,24 @@
 package com.bridgelabz.userapis.loginregistration.services;
 
-import java.net.URI;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import javax.servlet.ServletContext;
-import javax.ws.rs.Consumes;
+import javax.json.bind.JsonbException;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriBuilder;
-
-import org.glassfish.jersey.server.mvc.Viewable;
 
 import com.bridgelabz.userapis.loginregistration.database.DAOImplementation;
 import com.bridgelabz.userapis.loginregistration.database.DatabaseConnectivity;
 import com.bridgelabz.userapis.loginregistration.database.IUserDao;
 import com.bridgelabz.userapis.loginregistration.model.Users;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 /**
  * 
@@ -39,19 +33,19 @@ import com.google.gson.JsonArray;
 @Path("/usersRegistration")
 public class UsersRegistration 
 {
-	
+
 	public UsersRegistration() {
 	}
 
 	DatabaseConnectivity dtabaseConnectivity = new DatabaseConnectivity();
 	IUserDao daoObj = new DAOImplementation();
-	
+
 
 	@POST
 	@Path("/addUsers")
-//	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	public void registerUser(
+	@Produces({MediaType.APPLICATION_JSON}) //will produce JSON type
+//	@Produces("text/html")
+	public Response registerUser(
 			@FormParam("fname") String firstname,
 			@FormParam("lname") String lastname,
 			@FormParam("uname") String username,
@@ -61,7 +55,7 @@ public class UsersRegistration
 			@FormParam("state_name") String state,
 			@FormParam("pincode") String zip)
 	{
-		
+
 		List<Users> usersList = new ArrayList<Users>();
 		Users userInfo = new Users();
 		userInfo.setFirstname(firstname);
@@ -72,34 +66,36 @@ public class UsersRegistration
 		userInfo.setCity(city);
 		userInfo.setState(state);
 		userInfo.setZip(zip);
-		
-		
-		if(daoObj.insertIntoUserApiTable(userInfo))
-			System.out.println("Successfully Inserted");
-		usersList.add(userInfo);		
-	}
-	
 
-//	@GET
-//	@Path("/getAll")
-//	//@Consumes(MediaType.APPLICATION_JSON)
-////	@Produces(MediaType.TEXT_HTML)
-//	public JsonArray display() 
-//	{
-//		JsonArray userList = daoObj.getAll();
-//		System.out.println("List :"+userList);
-//		
-////		return new Viewable("/feed", model);
-//		return userList;
-//	}
+
+		if(daoObj.insertIntoUserApiTable(userInfo))
+		{
+			System.out.println("Successfully Inserted");
+//			String output = "<font face='verdana' size='2'>" +
+//	                "Web Service has added your Customer information with Name - <u>"+firstname+"</u>, Country - <u>"+state+"</u></font>";
+//	        return Response.status(200).entity(output).build();
+		}
+		usersList.add(userInfo);	
+		return Response.status(200).entity(usersList).build();
+	}
+
 	@GET
 	@Path("/getAll")
 	@Produces(MediaType.APPLICATION_JSON)
-    public String indexes() {
-//        Map<String, String> model = new HashMap<>();
+	public String indexes() 
+	{
 		JsonArray userList = daoObj.getAll();
 		System.out.println("List :"+userList);
-        return userList.toString();
-        		//new Viewable("/usersData.jsp", userList);
+		return userList.toString();
+		//new Viewable("/usersData.jsp", userList);
+	}
+	
+	@GET
+    @Path("{mail}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getCustomer(@PathParam("mail") String mail) throws JsonbException
+	{ 
+        return daoObj.getUserByName(mail);
     }
+	
 }
