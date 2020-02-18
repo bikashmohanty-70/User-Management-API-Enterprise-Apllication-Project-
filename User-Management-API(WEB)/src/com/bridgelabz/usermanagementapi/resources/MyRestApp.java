2bc34f91d527cea9e4032f7c2eaeedd5357e710b
bikashmohanty-70 <bikashmohanty70@gmail.com>
2020-02-18 11:26:31 +0530

    [Bikash Mohanty]Updated. Documentation done

--- User-Management-API(WEB)/src/com/bridgelabz/usermanagementapi/resources/MyRestApp.java
+++ User-Management-API(WEB)/src/com/bridgelabz/usermanagementapi/resources/MyRestApp.java
@@ -1,11 +1,10 @@
 package com.bridgelabz.usermanagementapi.resources;
 
-
-
-
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("api")
+@ApplicationPath("api") // This annotation identifies the application path that serves as the base URI
+						// for all resources. This should be used on the configuration class, i.e. the
+						// subclass of javax.ws.rs.core.Application.
 public class MyRestApp extends Application {
 }
